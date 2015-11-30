bacfcdd6b8b376694cb0a9a3c7e50328f6c7a940
ronualdo <raxmac@gmail.com>
2015-11-29 23:03:56 -0300

    initial commit
    default yeoman angular generated app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feedbackmeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the feedbackmeApp
+ */
+angular.module('feedbackmeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name feedbackmeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the feedbackmeApp
+ */
+angular.module('feedbackmeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
